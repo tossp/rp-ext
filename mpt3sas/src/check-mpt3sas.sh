18a3b3c4d630cfbf3936a2810ce36da4a6a66990
砼砼 <code@tossp.com>
2021-10-03 21:09:16 +0800

    aadd 42218

--- mpt3sas/src/check-mpt3sas.sh
+++ mpt3sas/src/check-mpt3sas.sh
@@ -5,6 +5,7 @@
 echo -n "Loading module MPT 3 SAS -> "
 
 if [ $(/sbin/lsmod |grep -i mpt3sas|wc -l) -gt 0 ] ; then 
-	echo "Module MPT 3 SAS loaded succesfully" 
+	echo "Module MPT 3 SAS loaded succesfully"
+	sleep 2 # we need to wait for disks to be detected so other processes don't randomly explode
 	else echo "Module MPT 3 SAS is not loaded "
 fi
